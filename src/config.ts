b95f635624945a7a1b133480d582d80058698db1
tomwright0 <47940073+tomwright0@users.noreply.github.com>
2025-01-09 16:21:20 +1030

    change to my domain

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://twright.net", // replace this with your deployed domain
+  website: "https://www.twright.net", // replace this with your deployed domain
   author: "Tom Wright",
   profile: "",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
